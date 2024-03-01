77ac511eb661c588e01ee813bc243144329fc785
Evan Chisholm <elchisholm248@gmail.com>
2024-02-29 22:23:23 -0500

    better scaling'

--- trace_vid.py
+++ trace_vid.py
@@ -8,7 +8,7 @@ m = ResNet()
 m.load_state_dict(torch.load('best_model.pt'))
 m.eval()
 
-video_path = './ski-video.mp4'
+video_path = './clipped.mp4'
 
 
 points = []
@@ -41,14 +41,18 @@ while True:
             'y': y2,
             'side': 'r'
         })
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
     
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Image Display Example")
 
 i = 0
 FPS = 24
 clock = pygame.time.Clock()
+render_surface = pygame.Surface((1920, 1080))
 
 while True:
     i = i % (len(pygame_frames))
@@ -58,10 +62,13 @@ while True:
 
     current_points = points[2*i:2*i+2]
     frame = pygame_frames[i]
-    screen.blit(frame, (0, 0))
+    render_surface.blit(frame, (0, 0))
     print(current_points)
     for point in current_points:
-        pygame.draw.circle(screen, (255, 0, 0) if point['side'] == 'l' else (0, 0, 255), (point['x'], point['y']), 10)
+        pygame.draw.circle(render_surface, (255, 0, 0) if point['side'] == 'l' else (0, 0, 255), (point['x'], point['y']), 10)
+    
+    scaled_render_surface = pygame.transform.scale(render_surface, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_render_surface, (0, 0))
 
     pygame.display.flip()
     clock.tick(FPS)
